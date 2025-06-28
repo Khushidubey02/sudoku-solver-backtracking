aa80bda4decfef7a010163aba4f5623b131375ba
Khushi Dubey <khushidubey3003@gmail.com>
2025-06-28 16:19:14 +0530

    Add files via upload

---
+++ Sudoku.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Sudoku {
+    static boolean solve(int[][] board) {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isSafe(board, r, c, num)) {
+                            board[r][c] = num;
+                            if (solve(board)) return true;
+                            board[r][c] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static boolean isSafe(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num ||
+                board[row - row % 3 + i / 3][col - col % 3 + i % 3] == num) return false;
+        }
+        return true;
+    }
+
+    static void print(int[][] board) {
+        for (int[] row : board) System.out.println(Arrays.toString(row));
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+        if (solve(board)) print(board);
+        else System.out.println("No solution exists");
+    }
+}
